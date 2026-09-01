532945ea240c7a14751b77879c00673f9de44cec
miguelcm-wq <miguel.cm@aluno.ifsc.edu.br>
2026-09-01 00:58:15 -0300

    Cuida do meu filho pra mim

--- jogo.py
+++ jogo.py
@@ -1,20 +1,6 @@
 plataformas = ['PC', 'Xbox', 'Playstation', 'Nintendo']
+jogos = []
 
-def jogos():
-    while(True):
-        menu_jogo = int(input('\n(1) - cadastrar\t(2) - listar\t(3) - sair\n Digite: '))
-
-        if (menu_jogo == 1):
-            cadastrar_jogo(plataformas)
-
-        elif (menu_jogo == 2):
-            pass
-
-        elif (menu_jogo == 3):
-            break
-
-def cadastrar_jogo(plataformas):
-    titulo = input('\nDigite o ttulo do jogo: ')
-    genero = input('Digite o gênero do jogo: ')
-    for indice, plataformas in enumerate(plataformas, start=1):
-        print(f"{indice} - {plataformas}")
+def cadastrar_jogo(titulo, plataformas, genero, valor_locacao):
+    jogo = {'titulo': titulo, 'plataforma': plataformas, 'genero': genero, 'valor_locacao': valor_locacao}
+    jogos.append(jogo)
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from jogo import jogos
+from jogo import cadastrar_jogo, plataformas
 
 while(True):
     menu = int(input('\n(1) - Jogo\n(2) - Cliente\n(3) - Locações\n(4) - sair\n Digite: '))
 
     if (menu == 1):
-        jogos()
+        while(True):
+                menu_jogo = int(input('\n(1) - cadastrar\n(2) - listar\n(3) - sair\n Digite: '))
+        
+                if (menu_jogo == 1):
+                    titulo = input('\nDigite o ttulo do jogo: ')
+                    genero = input('Digite o gênero do jogo: ')
+                    print('===== plataformas =====')
+                    for indice, plataformas in enumerate(plataformas, start=1):
+                        print(f"{indice} - {plataformas}")
+                    escolha_plataforma = int(input('Digite plataforma do jogo: '))-1
+                    valor_locacao = float(input('Digite o valor da locação do jogo: '))
+                    cadastrar_jogo(titulo, plataformas, genero, valor_locacao)
+                    break
+                
+                elif (menu_jogo == 2):
+                    pass
+                    break
+        
+                elif (menu_jogo == 3):
+                    break
+
+                else:
+                 print('opção inválida, Tente novamente.')   
 
     elif (menu == 2):
         pass
